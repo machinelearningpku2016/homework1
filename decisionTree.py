4e89cc48ceb7f7bf4e7f0307737ff1a4c23d391c
radar-bear <liangzibear@163.com>
2016-10-26 23:35:45 +0800

    prototype checked [except pruning function]
    The decisionTree can grow correctly(if I didn’t make mistakes). After a
    test, trained by the first 1600 instances and tested by the others, the
    correct rate can attain  96.6%. I already satisfied with the result.
    But I need write some more function to pruning this tree, it may
    improve the performance of this code.

--- decisionTree.py
+++ decisionTree.py
@@ -1,7 +1,7 @@
 import numpy as np
 from collections import Counter
 
-def entropy(list):
+def cal_entropy(list):
 	Count = Counter(list)
 	total = len(list)
 	entropy = 0
@@ -16,24 +16,24 @@ class treeNode(object):
 		self.target = None
 		self.instances_num = instances_num
 	#generate the child dict
-	def end(data):
+	def end(self, data):
 		#collect all instances' class in this node
-		classes = list(map(lambda i:data[i][len(data[i])], self.instances_num))
+		classes = list(map(lambda i:data[i][len(data[i])-1], self.instances_num))
 		#find the dominant one and change self.target from None to this class
 		self.target = Counter(classes).most_common(1)[0][0]
 	#check if all instances in this node are in same class
 	#if not, return False
 	#if true, return true, the node should be ended
-	def check_end(data):
-		for i in instances_num:
-			if not data[i][len(data[i])]==data[0][len(data[0])]:
+	def check_end(self, data):
+		for i in self.instances_num:
+			if not data[i][len(data[i])-1]==data[0][len(data[0])-1]:
 				return False
 		return True
-	def entropy(data, index_num=-1):
-		if index==-1:
-			classes = list(map(lambda i:data[i][len(data[i])], self.instances_num))
-			return entropy(classes)
-		elif not index_num in range(data[0]-1):
+	def entropy(self, data, index_num=-1):
+		if index_num==-1:
+			classes = list(map(lambda i:data[i][len(data[i])-1], self.instances_num))
+			return cal_entropy(classes)
+		elif not index_num in range(len(data[0])-1):
 			print("index_num overflow") #in case of wrong index_num
 		else:
 			#collect all instances' indexes
@@ -44,22 +44,24 @@ class treeNode(object):
 			#initial this dict
 			instances_class = {}
 			for i in indexes_value:
-				instances[i]=[]
+				instances_class[i]=[]
 			for i in self.instances_num:
-				instances_class[data[i][index_num]].append(data[i][len(data[i])])
+				instances_class[data[i][index_num]].append(data[i][len(data[i])-1])
 			entropy = 0
 			for i in instances_class:
-				entropy += len(instances_class[i])/len(self.instances_num)*entropy(instances_class[i])
+				entropy += len(instances_class[i])/len(self.instances_num)*cal_entropy(instances_class[i])
 			return entropy
-	def generateChild(data, index):
+	def generateChild(self, data, index):
 		child_instances={}
 		for i in self.instances_num:
 			if data[i][index] in child_instances:
 				child_instances[data[i][index]].append(i)
-			else child_instances[data[i][index]]=[i]
+			else:
+				child_instances[data[i][index]]=[i]
 		for i in child_instances:
 			self.children[i]=treeNode(self, child_instances[i])
-	def grow(data, index_list, threshold):
+	def grow(self, data, index_list, threshold):
+#		print(type(index_list))
 		if self.check_end(data) or len(index_list)==0:
 			self.end(data)
 			return 0
@@ -67,7 +69,7 @@ class treeNode(object):
 		#the var below storage the entropy change brought by each index
 		information_gain_ratio = {}
 		for i in index_list:
-			information_gain_ratio[i]=(node_entropy-self.entropy(data, i))/entropy(list(map(lambda i:data[i][index_num], self.instances_num)))
+			information_gain_ratio[i]=(node_entropy-self.entropy(data, i))/cal_entropy(list(map(lambda k:data[k][i], self.instances_num)))
 		max_infromation_gain_ratio = sorted(information_gain_ratio.items(), key = lambda k:k[1], reverse=True)[0]
 		if max_infromation_gain_ratio[1]<threshold:
 			self.end(data)
@@ -79,29 +81,40 @@ class treeNode(object):
 		for i in self.children:
 			self.children[i].grow(data, index_list, threshold)
 	#input instance x, use the tree make decision
-	def apply(x):
+	def apply(self, x):
 		#where x has a dimension as interpret
 		if self.target:
 			return self.target
 		return self.children[x[self.index_num]].apply(x)
 	def printTree(data):
-
-
-def makeDecisionTree(data):
+		pass
+def check_end_test(tree, data):
+	for i in tree.instances_num:
+		if not data[i][len(data[i])-1]==data[0][len(data[0])-1]:
+			return False
+	return True
+def makeDecisionTree(data, threshold):
 	#initial index interpret
 	#key:value -> column_in_inputdata: [choice1, choice2...]
 	index={}
 	#initial index_entropy
-	for index_num in range(len(data[0]-1)):
+	for index_num in range(len(data[0])-1):
 		index[index_num]=[]
 	for i in range(len(data)):
-		for index_num in range(len(data[0]-1)):
+		for index_num in range(len(data[0])-1):
 			if not data[i][index_num] in index[index_num]:
 				index[index_num].append(data[i][index_num])
 	#index set finished
-
-	decisionTree = treeNode(None, range(len(data)))
-	threshold = 0.1
+#	print (index)
+	decisionTree = treeNode(None, list(range(len(data))))
 	decisionTree.grow(data, index, threshold)
 	return decisionTree
 
+data=[line.replace("\n","").split(',') for line in open('car.data').readlines()]
+for threshold in list(map(lambda i:(i+1)*0.1,range(10))):
+	decisionTree = makeDecisionTree(data[:1600], threshold)
+	account = 0
+	for i in range(1600,len(data)):
+		if not decisionTree.apply(data[i])==data[i][len(data[i])-1]:
+			account += 1
+	print(threshold," ",account/len(data))
