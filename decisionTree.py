2949d8201b5e74ae27e38f3694448857cfecb158
radar-bear <liangzibear@163.com>
2016-10-28 22:23:37 +0800

    RealFinished

--- decisionTree.py
+++ decisionTree.py
@@ -19,7 +19,9 @@ class treeNode(object):
 		self.children = {}
 		self.index_num = None #the index_num will be use in this node
 		self.target = None
+		self.likely_target = None  #if the tree isn't grow completely, use likely target 
 		self.instances_num = instances_num
+		
 	#generate the child dict
 	def end(self, data):
 		#collect all instances' class in this node
@@ -66,7 +68,7 @@ class treeNode(object):
 		for i in child_instances:
 			self.children[i]=treeNode(self, child_instances[i])
 	def grow(self, data, index_list, threshold):
-#		print(type(index_list))
+		self.likely_target = Counter(list(map(lambda i:data[i][len(data[i])-1], self.instances_num))).most_common(1)[0][0]
 		if self.check_end(data) or len(index_list)==0:
 			self.end(data)
 			return 0
@@ -82,14 +84,45 @@ class treeNode(object):
 		#generate child tree by index max_infromation_gain_ratio[0]
 		self.generateChild(data, max_infromation_gain_ratio[0])
 		self.index_num=max_infromation_gain_ratio[0]
-		del index_list[max_infromation_gain_ratio[0]]
+		new_index_list=index_list.copy()
+		del new_index_list[max_infromation_gain_ratio[0]]
 		for i in self.children:
-			self.children[i].grow(data, index_list, threshold)
+			self.children[i].grow(data, new_index_list, threshold)
+	def purning(self, data, alpha):
+		before_purning = 0
+		for i in self.children:
+			before_purning += len(self.children[i].instances_num)*self.children[i].entropy(data)
+		before_purning += alpha*len(self.children)
+		after_purning = self.entropy(data)*len(self.instances_num)
+		if after_purning < before_purning:
+			print('cut')
+			self.children = {}
+			self.end(data)
+		for i in self.children:
+			self.children[i].purning(data,alpha)
+	#Check the tree's children and itself
+	#the instances in children should be classified correctly
+	#its target should be the maximum element in its instances
+	def self_check(self, data):
+		for i in self.children:
+			if not test_all_same(list(map(lambda k:data[k][self.index_num], self.children[i].instances_num)),i):
+				print('wrong instances')
+				print(i,' in ', self.index_num)
+				self.children[i].self_check(data)
+		if self.target:
+			a=list(map(lambda k:data[k][len(data[k])-1], self.instances_num))
+			if not Counter(a).most_common(1)[0][0]==self.target:
+				print('wrong target')
+				print(self.target, 'should be', Counter(a).most_common(1)[0][0])
+		print ('check passed')
 	#input instance x, use the tree make decision
 	def apply(self, x):
 		#where x has a dimension as interpret
 		if self.target:
 			return self.target
+		if not x[self.index_num] in self.children:
+			return self.likely_target
+#		print(self.index_num," ",x[self.index_num])
 		return self.children[x[self.index_num]].apply(x)
 	def printTree(data):
 		pass
@@ -116,9 +149,31 @@ def makeDecisionTree(data, threshold):
 	return decisionTree
 
 data=[line.replace("\n","").split(',') for line in open('car.data').readlines()]
-for threshold in list(map(lambda i:(i+1)*0.1,range(10))):
-	decisionTree = makeDecisionTree(data[:1600], threshold)
+train_list=[]
+check_list=[]
+test_list=[]
+for i in range(len(data)):
+	if i%7==0 :
+		check_list.append(i)
+	if i%7==1:
+		test_list.append(i)
+	else:
+		train_list.append(i)
+train_data=list(map(lambda k:data[k], train_list))
+check_data=list(map(lambda k:data[k], check_list))
+test_data=list(map(lambda k:data[k], test_list))
+for alpha in list(map(lambda i:(i+1)*0.2,range(2))):
+	decisionTree = makeDecisionTree(train_data, 0.1)
+	decisionTree.purning(data, alpha)
 	account = 0
-	for i in range(1600,len(data)):
-		if not decisionTree.apply(data[i])==data[i][len(data[i])-1]:
+	for i in range(len(check_data)):
+		if not decisionTree.apply(check_data[i])==check_data[i][len(check_data[i])-1]:
 			account += 1
+	print(alpha," ",account/(len(check_data)))
+#for threshold in list(map(lambda i:(i+1)*0.02,range(10))):
+#	decisionTree = makeDecisionTree(train_data, threshold)
+#	account = 0
+#	for i in range(len(check_data)):
+#		if not decisionTree.apply(check_data[i])==check_data[i][len(check_data[i])-1]:
+#			account += 1
+#	print(threshold," ",account/(len(check_data)))
